1582b6be1f1286b483acfde7fc58e5f5950b7944
Bryan <irwinb14@gmail.com>
2023-05-01 16:41:49 -0700

    analytics component

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Layout from '@/components/layout';
 import '@/styles/globals.css';
 import { Figtree } from 'next/font/google';
 import Head from 'next/head';
+import { Analytics } from '@vercel/analytics/react';
 
 const figtree = Figtree({ subsets: ['latin'] });
 
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }) {
       </Head>
       <Layout>
         <Component {...pageProps} />
+        <Analytics />
       </Layout>
     </div>
   );
